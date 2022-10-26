5c7ff0cd389a95392a023315e0c98fd3c5a160a5
sara4se <s.m.m.h.h.18@gmail.com>
2022-10-26 18:30:53 +0300

    Initial Commit

---
+++ Kedu/KeduApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeduApp.swift
+//  Kedu
+//
+//  Created by Sara Alhumidi on 01/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct KeduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
